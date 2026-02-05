e2ddbf6dbfa7f7f904ea0d91daec5d3cb219665b
patelmanasi <manasipatel0852@gmail.com>
2026-02-05 11:24:34 +0530

    Initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+/**
+ * This TestCase bootstraps a fake Laravel app
+ * for package testing
+ */
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Called before each test
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Any setup logic goes here
+    }
+
+    /**
+     * Register package service providers
+     * (Leave empty for now)
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            // Example:
+            // \Vendor\Package\PackageServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Setup test environment
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Use SQLite in-memory database for tests
+        $app['config']->set('database.default', 'testbench');
+
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+/**
+ * Simple test example
+ */
+class ExampleTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
