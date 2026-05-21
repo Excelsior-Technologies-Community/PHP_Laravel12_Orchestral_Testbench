9b6fd464ecb276a2d2d8564c3987371774f9be92
savasanisujal1 <savasanisujal1@gmail.com>
2026-05-21 16:05:26 +0530

    added MakeTestbenchFeature command, configured Orchestral Testbench TestCase, and added UserLoginTest

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\MakeTestbenchFeature;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('make:testbench-feature {name}', function ($name) {
+    (new MakeTestbenchFeature())->handle();
+})->purpose('Create a new feature test class in tests/Feature');
